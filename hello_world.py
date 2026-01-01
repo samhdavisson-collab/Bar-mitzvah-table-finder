5c836517a641d83a293805a0974f147f59c550b4
samhdavisson-collab <sam@Sams-MacBook-Air.local>
2026-01-01 10:58:54 -0800

    added search by first name

--- hello_world.py
+++ hello_world.py
@@ -13,7 +13,18 @@ with open('data.csv', mode='r', newline='', encoding='utf-8') as file:
 
 def find_by_last_name(last_name):
     return [g for g in guests if last_name.lower() in g["last_name"].lower() ]
+def find_by_first_name(first_name):
+    return [g for g in guests if first_name.lower() in g["first_name"].lower() ]
 
-matches = find_by_last_name(st.text_input("Your last name:"))
-for guest in matches:
-    st.write(f"{guest['first_name']} {guest['last_name']} - {guest['table']}")
+
+# selectbox = "Search by " + st.selectbox("", ["first name", "last name"], width=200)
+selectbox = st.toggle("Seatch by first name")
+
+if not selectbox:# == "Search by last name":
+    matches = find_by_last_name(st.text_input("Your last name:"))
+    for guest in matches:
+        st.write(f"{guest['first_name']} {guest['last_name']} - {guest['table']}")
+else:
+    matches = find_by_first_name(st.text_input("Your first name:"))
+    for guest in matches:
+        st.write(f"{guest['first_name']} {guest['last_name']} - {guest['table']}")
